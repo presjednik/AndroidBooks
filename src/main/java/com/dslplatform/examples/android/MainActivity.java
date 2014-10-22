74d5b57d449dcd9c5efccc5f8c714605c0468e21
Drazen Bandic <drazen@element.hr>
2014-10-22 14:25:50 +0200

    Displaying all books:  * added 'border.xml' for drawing the border around a book  * added a 'book_layout.xml' which represents the layout for a single book  * added BookArrayAdapter class which populates the ListView with books  * changed the 'activity_main.xml' to hold a <ListView/> element

--- src/main/java/com/dslplatform/examples/android/MainActivity.java
+++ src/main/java/com/dslplatform/examples/android/MainActivity.java
@@ -3,10 +3,14 @@ package com.dslplatform.examples.android;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.dslplatform.examples.android.R;
+import com.dslplatform.examples.android.AndroidBooks.Book;
+import com.dslplatform.examples.android.utilities.BookArrayAdapter;
 
 import org.slf4j.Logger;
 
@@ -14,13 +18,26 @@ import com.dslplatform.client.Bootstrap;
 import com.dslplatform.patterns.ServiceLocator;
 
 public class MainActivity extends Activity {
+	
+	public static ServiceLocator locator;
+	private ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		listView = (ListView) findViewById(R.id.list_books);
+		
 		try {
-			final ServiceLocator locator = init();
+			locator = init();
+			
+			//get all books from the database
+			final List<Book> bookList = Book.search();
+			final BookArrayAdapter bookAdapter = new BookArrayAdapter(this,
+			R.layout.book_layout, bookList);
+			listView.setAdapter(bookAdapter);
+			listView.setTextFilterEnabled(true);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
