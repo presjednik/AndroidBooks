79882302b02f04f7d6636a92aa2ed1fad350e234
Drazen Bandic <drazen@element.hr>
2014-10-23 15:12:43 +0200

    Enabled adding new reviews for a book:  * added new 'add_review_layout.xml' which is the dialog for adding new reviews using  * new methods in ReviewUtility for adding reviews to database and displaying the alert dialog  * new strings added to 'strings.xml'

--- src/main/java/com/dslplatform/examples/android/utilities/ReviewUtility.java
+++ src/main/java/com/dslplatform/examples/android/utilities/ReviewUtility.java
@@ -1,5 +1,6 @@
 package com.dslplatform.examples.android.utilities;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -8,7 +9,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.dslplatform.examples.android.MainActivity;
 import com.dslplatform.examples.android.R;
@@ -18,6 +23,10 @@ import com.dslplatform.examples.android.AndroidBooks.repositories.BookRepository
 import com.dslplatform.patterns.Specification;
 
 public class ReviewUtility {
+
+	private static Spinner spinnerRating;
+	private static EditText editReviewedBy;
+	private static EditText editComment;
 
 	/**
 	 * 
@@ -65,8 +74,79 @@ public class ReviewUtility {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 					}
+				})
+				.setPositiveButton(R.string.add_review, new OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// creates a new AddDialogFragment for adding a review
+						createAddReviewDialog(isbn, context).show();
+					}
 				});
 		// show the dialog
 		builder.create().show();
+	}
+
+	/**
+	 * Creates a new AlertDialog with which you can add a new review.
+	 * 
+	 * @param isbn
+	 *            - isbn of the book we are updating
+	 * @param context
+	 *            - activity context which called the method
+	 * @return AlertDialog for adding new review for according book
+	 */
+	private static AlertDialog createAddReviewDialog(final String isbn,
+			final Activity context) {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		final LayoutInflater inflater = context.getLayoutInflater();
+		final View v = inflater.inflate(R.layout.add_review_layout, null);
+
+		spinnerRating = (Spinner) v.findViewById(R.id.spinner_rating);
+		editReviewedBy = (EditText) v.findViewById(R.id.edit_reviwedBy);
+		editComment = (EditText) v.findViewById(R.id.edit_comment);
+
+		builder.setView(v).setTitle("Add review")
+				.setNegativeButton(R.string.close, null)
+				.setPositiveButton(R.string.add_review, new OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						addReviewToDatabase(isbn);
+					}
+				});
+		return builder.create();
+	}
+
+	/**
+	 * Adds a new review to the database. First it uses a Specification to get
+	 * the book to which we are going to add the review, than it gets the
+	 * reviews, adds the new one and at the end it updates the book.
+	 *
+	 * @param isbn
+	 *            - isbn of the book to which we are adding a new review
+	 */
+	private static void addReviewToDatabase(final String isbn) {
+
+		// specification for getting the book
+		final BookRepository bookRepository = new BookRepository(
+				MainActivity.locator);
+		final Specification<Book> isbnEquals = new Book.isbnEquals(isbn);
+		final Future<List<Book>> futureResults = bookRepository
+				.search(isbnEquals);
+
+		// creating new Review that needs to be added
+		final Review review = new Review();
+		review.setRating(Integer.parseInt(String.valueOf(spinnerRating
+				.getSelectedItem())));
+		review.setReviewdBy(editReviewedBy.getText().toString());
+		review.setComment(editComment.getText().toString());
+
+		try {
+			// getting the book, adding the review and updating
+			final Book book = futureResults.get().get(0);
+			book.getReviews().add(review);
+			book.update();
+		} catch (InterruptedException | ExecutionException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
